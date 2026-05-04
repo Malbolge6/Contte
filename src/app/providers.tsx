38f71b12050d19e78432266095ea74f444ba0347
Malbolge6 <santosmalbolge@gmail.com>
2026-05-03 21:15:35 -0300

    Remove MIA and update providers

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,14 +2,12 @@
 
 import { SessionProvider } from 'next-auth/react'
 import { ThemeProvider } from '@/contexts/ThemeContext'
-import { MIA } from '@/components/mia/MIA'
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <SessionProvider>
       <ThemeProvider>
         {children}
-        <MIA />
       </ThemeProvider>
     </SessionProvider>
   )
